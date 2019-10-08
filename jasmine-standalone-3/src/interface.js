2f1692b659974bface65945e36ca179c23554c46
Oliver <ofcharrison@gmail.com>
2019-10-08 11:28:16 +0100

    small changes to merge

--- jasmine-standalone-3/src/interface.js
+++ jasmine-standalone-3/src/interface.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
       //Do the checks here.
       console.log(event)
       var film = event.target[0].value
+<<<<<<< HEAD
       var year = event.target[1].value
       console.log(year)
 
@@ -87,6 +88,44 @@ $(document).ready(function() {
         };
       });
     });
+=======
+
+         $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(film) + '&apikey=78eae46').then(function(response){
+           $('#moviescoreIMDB').text("")
+           $('#moviescoreRT').text("")
+           $('#moviescoreMC').text("")
+           $('img').attr('src');
+
+           var title = response.Title
+           var score = response.Ratings
+           var plot = response.Plot
+           var imdbvotes = response.imdbVotes
+           var poster = response.Poster
+           var imdbID = response.imdbID
+
+           $('#movietitle').text(title)
+           $('#movieplot').text(plot)
+           $('img').attr('src', poster);
+
+           if (score.length === 1)
+            {$('#moviescoreIMDB').text(score[0].Source + " Score: " + score[0].Value + " - Votes:" + imdbvotes)}
+           else if (score.length === 2)
+            {$('#moviescoreIMDB').text(score[0].Source + " Score: " + score[0].Value + " - Votes:" + imdbvotes)
+             $('#moviescoreRT').text(score[1].Source + " " + score[1].Value)}
+           else if (score.length === 3)
+             {$('#moviescoreIMDB').text(score[0].Source + " Score: " + score[0].Value + " - Votes:" + imdbvotes)
+             $('#moviescoreRT').text(score[1].Source + " Score: " + score[1].Value)
+             $('#moviescoreMC').text(score[2].Source + " Score: " + score[2].Value)}
+           else
+             {$('#moviescoreIMDB').text('No Score')};
+
+             $("#image").wrap($('<a>',{
+               href: 'https://www.imdb.com/title/' + imdbID + '/?ref_=fn_al_tt_1'
+             }));
+             $("#clickimage").text("(click image to go to IMDB)")
+         });
+       });
+>>>>>>> 88a4563df937436bf6fb5a48a8bbf32029c6acdc
 
 
   document.getElementById("searchfilm").addEventListener("submit", function(event){
